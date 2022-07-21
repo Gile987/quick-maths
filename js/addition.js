1d58459d782410395817cc8f6653f2a3a6f5e6fb
Mladen Reljic <mladen.reljic@noubis.com>
2022-07-21 16:07:26 +0200

    Format the code properly this time

--- js/addition.js
+++ js/addition.js
@@ -1,50 +1,54 @@
-const optionOne = document.querySelector('#option1');
-const optionTwo = document.querySelector('#option2');
-const optionThree = document.querySelector('#option3');
-const errorAudio = document.querySelector('#error');
-const correctAudio = document.querySelector('#correct');
+const optionOne = document.querySelector("#option1");
+const optionTwo = document.querySelector("#option2");
+const optionThree = document.querySelector("#option3");
+const errorAudio = document.querySelector("#error");
+const correctAudio = document.querySelector("#correct");
 
-let answer =  0;
+let answer = 0;
 
 const generateEquation = () => {
-    let firstNumber = Math.floor(Math.random() * 10);
-    let secondNumber = Math.floor(Math.random() * 10);
-    let wrongAnswerOne = Math.floor(Math.random() * 10);
-    let wrongAnswerTwo = Math.floor(Math.random() * 10);
-    let allAnswers = [];
+  let firstNumber = Math.floor(Math.random() * 10);
+  let secondNumber = Math.floor(Math.random() * 10);
+  let wrongAnswerOne = Math.floor(Math.random() * 10);
+  let wrongAnswerTwo = Math.floor(Math.random() * 10);
+  let allAnswers = [];
 
-    answer = firstNumber + secondNumber;
+  answer = firstNumber + secondNumber;
 
-    if (wrongAnswerOne != wrongAnswerTwo && wrongAnswerOne != answer && wrongAnswerTwo != answer) {
-      document.querySelector('#num1').innerHTML = firstNumber;
-      document.querySelector('#num2').innerHTML = secondNumber;
+  if (
+    wrongAnswerOne != wrongAnswerTwo &&
+    wrongAnswerOne != answer &&
+    wrongAnswerTwo != answer
+  ) {
+    document.querySelector("#num1").innerHTML = firstNumber;
+    document.querySelector("#num2").innerHTML = secondNumber;
 
-      allAnswers = [answer, wrongAnswerOne, wrongAnswerTwo];
-      allAnswers.sort(() => Math.random() - 0.5);
+    allAnswers = [answer, wrongAnswerOne, wrongAnswerTwo];
+    allAnswers.sort(() => Math.random() - 0.5);
 
-      optionOne.innerHTML = allAnswers[0];
-      optionTwo.innerHTML = allAnswers[1];
-      optionThree.innerHTML = allAnswers[2];
-    } else {
-      generateEquation();
-    };
+    optionOne.innerHTML = allAnswers[0];
+    optionTwo.innerHTML = allAnswers[1];
+    optionThree.innerHTML = allAnswers[2];
+  } else {
+    generateEquation();
+  }
 };
 
 const checkAnswer = (event) => {
-    if (event.target.innerHTML == answer) {
-        correctAudio.pause();
-        correctAudio.currentTime = 0;
-        correctAudio.play();
-        generateEquation();
-    } else {
-        errorAudio.pause();
-        errorAudio.currentTime = 0;
-        errorAudio.play();
-    }
-}
+  if (event.target.innerHTML == answer) {
+    correctAudio.pause();
+    correctAudio.currentTime = 0;
+    correctAudio.play();
+    generateEquation();
+  } else {
+    errorAudio.pause();
+    errorAudio.currentTime = 0;
+    errorAudio.play();
+  }
+};
 
-optionOne.addEventListener('click', checkAnswer);
-optionTwo.addEventListener('click', checkAnswer);
-optionThree.addEventListener('click', checkAnswer);
+optionOne.addEventListener("click", checkAnswer);
+optionTwo.addEventListener("click", checkAnswer);
+optionThree.addEventListener("click", checkAnswer);
 
 generateEquation();
