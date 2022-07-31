a0f735fcfbc25bfa3bbf053449e08fe05f95736d
gile987 <mladen.reljic87@gmail.com>
2022-07-31 12:35:47 +0200

    Rewrite the script to properly toggle the class on click

--- js/achis.js
+++ js/achis.js
@@ -1,8 +1,6 @@
 const achisToggle = document.querySelector("#achis");
 const achis = document.querySelector(".achis_holder");
 
-showAchis = () => {
-  achis.style.display = "block";
-};
-
-achisToggle.addEventListener("click", showAchis);
+achisToggle.addEventListener("click", () => {
+  achis.classList.toggle("show");
+});
