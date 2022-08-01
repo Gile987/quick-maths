8691be03fddaabd83d0ab5b37656992f8996ba75
gile987 <mladen.reljic87@gmail.com>
2022-08-01 15:20:51 +0200

    Change the structure of the project a bit, add the start of the logic for achievements

--- js/achis.js
+++ js/achis.js
@@ -1,5 +1,21 @@
 const achisToggle = document.querySelector("#achis");
 const achis = document.querySelector(".achis_holder");
+const points = document.querySelector("#points");
+const achiTenPoints = document.querySelector("#ten_points");
+
+let score = 9;
+
+const addPoints = (num) => {
+  score = score + num;
+  points.innerHTML = score;
+  console.log(score);
+
+  switch (score) {
+    case 10:
+      achiTenPoints.classList.add("receive_achi");
+      break;
+  }
+};
 
 achisToggle.addEventListener("click", () => {
   achis.classList.toggle("show");
--- js/math.js
+++ js/math.js
@@ -3,7 +3,6 @@ const optionTwo = document.querySelector("#option2");
 const optionThree = document.querySelector("#option3");
 const errorAudio = document.querySelector("#error");
 const correctAudio = document.querySelector("#correct");
-const points = document.querySelector("#points");
 
 const add = document.querySelector("#add");
 const sub = document.querySelector("#sub");
@@ -11,7 +10,6 @@ const mul = document.querySelector("#mul");
 const div = document.querySelector("#div");
 
 let answer = 0;
-let score = 0;
 
 const changeClass = (event) => {
   let currentClass = document.querySelector(".current");
@@ -109,11 +107,6 @@ const generateEquation = () => {
   optionOne.innerHTML = allAnswers[0];
   optionTwo.innerHTML = allAnswers[1];
   optionThree.innerHTML = allAnswers[2];
-};
-
-const addPoints = (num) => {
-  score = score + num;
-  points.innerHTML = score;
 };
 
 const checkAnswer = (event) => {
