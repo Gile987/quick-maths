59a74eef3e66de3575c49059cf43b85556e45187
gile987 <mladen.reljic87@gmail.com>
2022-08-01 15:39:15 +0200

    Chain if statements to count points for achievements - WIP

--- js/achis.js
+++ js/achis.js
@@ -2,19 +2,24 @@ const achisToggle = document.querySelector("#achis");
 const achis = document.querySelector(".achis_holder");
 const points = document.querySelector("#points");
 const achiTenPoints = document.querySelector("#ten_points");
+const achiTwentyPoints = document.querySelector("#twenty_points");
+const minusTen = document.querySelector("#minus_ten");
 
-let score = 9;
+let score = 0;
 
 const addPoints = (num) => {
   score = score + num;
   points.innerHTML = score;
-  console.log(score);
-
-  switch (score) {
-    case 10:
-      achiTenPoints.classList.add("receive_achi");
-      break;
-  }
+  if (score >= 10) {
+    console.log("10 points");
+    achiTenPoints.classList.add("receive_achi");
+  } if (score >= 20) {
+    console.log("20 points");
+    achiTwentyPoints.classList.add("receive_achi");
+  } if (score <= -10) {
+    console.log("Game Over");
+    minusTen.classList.add("receive_achi");
+  };
 };
 
 achisToggle.addEventListener("click", () => {
