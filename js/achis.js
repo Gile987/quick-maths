c8e9d4a534f516b2d45b8b75ff25fdf37697e5c1
gile987 <mladen.reljic87@gmail.com>
2022-08-02 17:46:42 +0200

    Add a clear localStorage button for testing, start working on a basic state machine for score and achievements

--- js/achis.js
+++ js/achis.js
@@ -4,40 +4,67 @@ const points = document.querySelector("#points");
 const achiTenPoints = document.querySelector("#ten_points");
 const achiTwentyPoints = document.querySelector("#twenty_points");
 const minusTen = document.querySelector("#minus_ten");
+const clear = document.querySelector("#clear");
 
 let score = 0;
-
-const addPoints = (num) => {
-  score = score + num;
-  points.innerHTML = score;
-
-  if (score >= 10) {
-    console.log("10 points");
-    achiTenPoints.classList.add("receive_achi");
-  } if (score >= 20) {
-    console.log("20 points");
-    achiTwentyPoints.classList.add("receive_achi");
-  } if (score <= -10) {
-    console.log("Game Over");
-    minusTen.classList.add("receive_achi");
-  };
-  highScore();
-};
 
 achisToggle.addEventListener("click", () => {
   achis.classList.toggle("show");
 });
 
-const highScore = () => {
-  let highScore = localStorage.getItem("highScore");
-  if (highScore === null) {
-    highScore = 0;
-  }
-  if (score > highScore) {
-    localStorage.setItem("highScore", score);
-    highScore = score;
-  }
-  document.querySelector("#high_score").innerHTML = highScore;
+const state = {
+  score: 0,
+  highScore: 0,
+  points: {
+    10: "ten_points",
+    20: "twenty_points",
+  },
+  achi: {
+    ten_points: false,
+    twenty_points: false,
+  },
 };
 
+const checkAchi = () => {
+  const { score, points, achi } = state;
+  Object.keys(points).forEach((key) => {
+    console.log(localStorage)
+    const achiStatus = localStorage.getItem(points[key]);
+    if (achiStatus === null) {
+      achi[points[key]] = false;
+    } else {
+      achi[points[key]] = achiStatus;
+    };
+    if (score >= key) {
+      achi[points[key]] = true;
+    };
+    if (achi[points[key]]) {
+      document.querySelector(`#${points[key]}`).classList.add("receive_achi");
+      localStorage.setItem(points[key], true);
+    };
+  });
+};
+
+const addPoints = (num) => {
+  state.score = state.score + num;
+  points.innerHTML = state.score;
+  highScore();
+  checkAchi();
+};
+
+const highScore = () => {
+  state.highScore = localStorage.getItem("highScore");
+  if (state.score > state.highScore) {
+    localStorage.setItem("highScore", state.score);
+    state.highScore = state.score;
+  }
+  document.querySelector("#high_score").innerHTML = state.highScore;
+};
+
+const clearClick = () => {
+  localStorage.clear();
+};
+clear.addEventListener("click", clearClick);
+
 highScore();
+checkAchi();
