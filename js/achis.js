07dba1732a0c689433ae1aab91e70afac2381f0e
gile987 <mladen.reljic87@gmail.com>
2022-08-05 17:40:39 +0200

    Change the logic so that the ten point combo actually works properly

--- js/achis.js
+++ js/achis.js
@@ -15,7 +15,7 @@ achisToggle.addEventListener("click", () => {
 const state = {
   score: 0,
   highScore: 0,
-  previousScore: 0,
+  counter: 0,
   points: {
     10: "ten_points",
     20: "twenty_points",
@@ -35,16 +35,24 @@ const checkAchi = () => {
       localStorage.setItem("achi", JSON.stringify(state.achi));
     };
   };
-  if (state.score - state.previousScore >= 10) {
-    state.achi.ten_point_combo = true;
-    document.querySelector("#ten_point_combo").classList.add("receive_achi");
-    localStorage.setItem("achi", JSON.stringify(state.achi));
-  };
 };
 
 const addPoints = (num) => {
   state.score = state.score + num;
   points.innerHTML = state.score;
+
+  if (num === -1) {
+    state.counter = 0;
+  } else if (num === 1) {
+    state.counter = state.counter + 1;
+  };
+
+  if (state.counter === 10) {
+    state.achi.ten_point_combo = true;
+    document.querySelector("#ten_point_combo").classList.add("receive_achi");
+    localStorage.setItem("achi", JSON.stringify(state.achi));
+  };
+
   highScore();
   checkAchi();
 };
